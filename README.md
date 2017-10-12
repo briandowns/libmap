a98a411027a33298298bc4a1fe1f6a725df8b19b
Brian Downs <brian.downs@gmail.cm>
2017-10-12 11:25:03 -0700

    updates

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ libmap is a simple library to proide a map implementation. They keys and values 
 
 ### Create a new map
 
-```
+```c
 map_t* m = new_map(100);
 ```
 
 ### Add some values
 
-```
+```c
 set(m, "first", "Brian");
 int age = 99;
 set(m, "age", &age);
@@ -20,7 +20,7 @@ set(m, "age", &age);
 
 ### Retrieve and print the values
 
-```
+```c
 printf("first: %s\n", get(m, "first"));  
 void* a = get(m, "age");
 printf("age: %d\n", *((int*)a));
@@ -28,10 +28,6 @@ printf("age: %d\n", *((int*)a));
 
 ### Free the memory used by the map
 
-```
+```c
 free_map(m);
 ```
-
-## TODO
-
-- [ ] add support for buckets
--- map.c
+++ map.c
@@ -4,48 +4,48 @@
 #include "map.h"
 
 /** new_map creates a new map  with the given size */
-map_t *new_map(int size) {
-    map_t* m = calloc(1, sizeof(map_t));
-    m->keys = calloc(size, sizeof(void*));
-    m->values = calloc(size, sizeof(void*));
-    m->size = size;
-    return m;
+map_t* new_map(int size) {
+  map_t* m = calloc(1, sizeof(map_t));
+  m->keys = calloc(size, sizeof(void*));
+  m->values = calloc(size, sizeof(void*));
+  m->size = size;
+  return m;
 }
 
 /** free_map takes the given map and frees the memory it uses */
 void free_map(map_t* m) {
-    free(m->keys);
-    free(m->values);
-    free(m);
+  free(m->keys);
+  free(m->values);
+  free(m);
 }
 
 /** key_idx retrieves the index of the given key */
 int key_idx(map_t* m, void* key) {
-    int i = (int)key % m->size;
-    while (m->keys[i] && m->keys[i] != key) {
-        i = (i + 1) % m->size;
-    }
-    return i;
+  int i = (int)key % m->size;
+  while (m->keys[i] && m->keys[i] != key) {
+    i = (i + 1) % m->size;
+  }
+  return i;
 }
 
 /** set adds the key and it's value to the given map */
 void set(map_t* m, void* key, void* value) {
-    int i = key_idx(m, key);
-    m->keys[i] = key;
-    m->values[i] = value;
+  int i = key_idx(m, key);
+  m->keys[i] = key;
+  m->values[i] = value;
 }
 
 /** get retrieves the given key from the map */
 void* get(map_t* m, void* key) {
-    int i = key_idx(m, key);
-    return m->values[i];
+  int i = key_idx(m, key);
+  return m->values[i];
 }
 
-/** del removes the given key and associated value 
+/** del removes the given key and associated value
 from the given map */
 void del(map_t* m, void* key) {
-    int i = key_idx(m, key);
-    size_t l = sizeof(m->keys) / sizeof(void*);
-    m->keys[i] = m->keys[l-1];
-    m->values[i] = m->values[l-1];
+  int i = key_idx(m, key);
+  size_t l = sizeof(m->keys) / sizeof(void*);
+  m->keys[i] = m->keys[l - 1];
+  m->values[i] = m->values[l - 1];
 }
