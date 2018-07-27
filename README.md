2dba5cd83b85303b88fe4a758df33a39e39de7fc
Brian Downs <brian.downs@gmail.com>
2018-07-27 13:20:10 -0700

    rewrite names

--- README.md
+++ README.md
@@ -7,27 +7,27 @@ libmap is a simple library to proide a map implementation. They keys and values 
 ### Create a new map
 
 ```c
-map_t* m = new_map(100);
+map_t* m = map_new(100);
 ```
 
 ### Add some values
 
 ```c
-set(m, "first", "Brian");
+map_set(m, "first", "Brian");
 int age = 99;
-set(m, "age", &age);
+map_set(m, "age", &age);
 ```
 
 ### Retrieve and print the values
 
 ```c
 printf("first: %s\n", get(m, "first"));  
-void* a = get(m, "age");
+void* a = map_get(m, "age");
 printf("age: %d\n", *((int*)a));
 ```
 
 ### Free the memory used by the map
 
 ```c
-free_map(m);
+map_free(m);
 ```
--- map.h
+++ map.h
@@ -1,12 +1,19 @@
-/** map_t is the data structure to store keys and values */
+#ifndef _MAP_H
+#define _MAP_H
+
+/** 
+ * map_t is the data structure to store keys and values 
+ */
 typedef struct {
     int size;
     void **keys;
     void **values;
 } map_t;
 
-map_t *new_map(int size);
-void free_map(map_t* h);
-void* get(map_t* h, void* key);
-void set(map_t* h, void* key, void* value);
-void del(map_t* m, void* key);
+map_t *map_new(int size);
+void map_free(map_t* h);
+void *map_get(map_t* h, void* key);
+void map_set(map_t* h, void* key, void* value);
+void map_del(map_t* m, void* key);
+
+#endif
