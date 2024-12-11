74bcf2c91d2d6aa55b5f91ea13daf753b21b841f
Brian Downs <brian.downs@gmail.com>
2024-12-11 16:37:10 -0700

    updates
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- map.c
+++ map.c
@@ -1,23 +1,51 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2024 Brian J. Downs
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "map.h"
 
-/** 
- * map_new creates a new map  with the given size 
- */
-struct Map* map_new(int size) {
-    struct Map *m = calloc(1, sizeof(struct Map));
+
+map_t*
+map_new(const int size)
+{
+    map_t *m = calloc(1, sizeof(map_t));
     m->keys = calloc(size, sizeof(void*));
     m->values = calloc(size, sizeof(void*));
     m->size = size;
+
     return m;
 }
 
-/** 
- * map_free takes the given map and frees the memory it uses 
- */
-void map_free(struct Map *m) {
+
+void
+map_free(map_t *m)
+{
     free(m->keys);
     free(m->values);
     free(m);
@@ -26,38 +54,43 @@ void map_free(struct Map *m) {
 /** 
  * key_idx retrieves the index of the given key 
  */
-int key_idx(struct Map *m, void *key) {
+static int
+key_idx(map_t *m, const void *key)
+{
     int i = (int)key % m->size;
     while (m->keys[i] && m->keys[i] != key) {
         i = (i + 1) % m->size;
     }
+
     return i;
 }
 
-/** 
- * map_set adds the key and it's value to the given map 
- */
-void map_set(struct Map *m, void *key, void *value) {
+
+void
+map_set(map_t *m, const void *key, const void *value)
+{
     int i = key_idx(m, key);
+
     m->keys[i] = key;
     m->values[i] = value;
 }
 
-/** 
- * map_get retrieves the given key from the map 
- */
-void* map_get(struct Map *m, void *key) {
+
+void*
+map_get(map_t *m, const void *key)
+{
     int i = key_idx(m, key);
+
     return m->values[i];
 }
 
-/** 
- * map_del removes the given key and associated value 
- * from the given map 
- */
-void map_del(struct Map *m, void *key) {
+
+void
+map_del(map_t *m, const void *key)
+{
     int i = key_idx(m, key);
     size_t l = sizeof(m->keys) / sizeof(void*);
+
     m->keys[i] = m->keys[l - 1];
     m->values[i] = m->values[l - 1];
 }
--- map.h
+++ map.h
@@ -1,19 +1,72 @@
-#ifndef _MAP_H
-#define _MAP_H
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2024 Brian J. Downs
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef __MAP_H
+#define __MAP_H
 
 /** 
- * Map is the data structure to store keys and values 
+ * data structure to store keys and values 
  */
-struct Map {
+typedef struct
+{
     int size;
     void **keys;
     void **values;
-};
+} map_t;
 
-struct Map *map_new(int size);
-void map_free(struct Map *h);
-void *map_get(struct Map *h, void *key);
-void map_set(struct Map *h, void *key, void *value);
-void map_del(struct Map *m, void *key);
+/** 
+ * map_new creates a new map  with the given size 
+ */
+map_t*
+map_new(const int size);
 
-#endif
+/** 
+ * map_free takes the given map and frees the memory it uses 
+ */
+void
+map_free(map_t *h);
+
+/** 
+ * map_get retrieves the given key from the map 
+ */
+void*
+map_get(map_t *h, const void *key);
+
+/** 
+ * map_set adds the key and it's value to the given map 
+ */
+void
+map_set(map_t *h, const void *key, const void *value);
+
+/** 
+ * map_del removes the given key and associated value 
+ * from the given map 
+ */
+void
+map_del(map_t *m, const void *key);
+
+#endif /** end __MAP_H */
